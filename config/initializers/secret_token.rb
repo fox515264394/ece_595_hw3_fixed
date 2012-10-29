9f78a1fc4817686c39c5130c1d9d1140a000ed23
fox515264394 <yuan.zhihao@yahoo.com.cn>
2012-10-29 16:33:24 -0600

    going to modify smarter cart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZYJewlery::Application.config.secret_token = 'd5c39ef2c4e2f87c23aea3b5ef53bdf88f1b144d2070c59bf62c8cd3904bbb7169b07ac10bcb0625c2a1359b6199cc163df87e56a8a83766d7c4edfe936392e1'
